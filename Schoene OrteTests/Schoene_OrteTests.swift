dd755629dc43ed92c1be2b8ae36429ae255ed22b
sebastianrieger <sebastian2406@t-online.de>
2020-04-18 15:56:22 +0200

    Initial Commit

---
+++ Schoene OrteTests/Schoene_OrteTests.swift
@@ -0,0 +1,34 @@
+//
+//  Schoene_OrteTests.swift
+//  Schoene OrteTests
+//
+//  Created by Sebastian Rieger on 18.04.20.
+//  Copyright © 2020 Sebastian Rieger. All rights reserved.
+//
+
+import XCTest
+@testable import Schoene_Orte
+
+class Schoene_OrteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
